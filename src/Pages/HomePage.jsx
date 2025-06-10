1f84107e5e3901f86df23454ea824b986879e281
ayesha <sayyadshakera@gmail.com>
2025-06-10 17:19:16 +0530

    fourth commit

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,6 +1,9 @@
 // src/pages/Home.jsx
 import React from 'react';
 import './HomePage.css';
+import Img1 from '../assets/vck.png';
+import Img2 from '../assets/vck.jpg';
+import Img3 from '../assets/vckstd.jpg';
 
 const Home = () => {
   return (
@@ -10,7 +13,7 @@ const Home = () => {
           <img
             alt="Vivekanand College Campus"
             className="hero-banner-image"
-            src="src\assets\vck.png"
+            src={Img1}
           />
           
         </div>
